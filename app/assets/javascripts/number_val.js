0345247577f934a11b3bc6b12f8afe38b2360e8a
Michael <Michael.ginalick@gmail.com>
2015-07-24 21:55:17 -0500

    modified js function

--- app/assets/javascripts/number_val.js
+++ app/assets/javascripts/number_val.js
@@ -8,18 +8,10 @@ $(document).ready(function(){
 	});
 
 		$('#continue').submit(function(event){
-			var height = $('.lead').height();
-			var width = $('.lead').width();
 			if($('#phone-number').val().length < 10){
-				freezePhoneDiv(event, height, width);
-
 				$('.lead').text("Number must be ten digits.");
-				restorePhoneDiv();
-				
 			} else if ($('#phone-number').val().charAt(0) === "1"){
-				freezePhoneDiv(event, height, width);
 				$('.lead').text("Area code and number only, please.");
-				restorePhoneDiv();
 			} else {
 				$(this).unbind('submit');
 			}
