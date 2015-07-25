c9c4ac055572fd0de05d08c498b435b06297ad39
Michael <Michael.ginalick@gmail.com>
2015-07-24 22:02:30 -0500

    added id to form for jquery

--- app/assets/javascripts/number_val.js
+++ app/assets/javascripts/number_val.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
 
 
-	$('#phone-number, #pin-number').keypress(function(event){
+	$('#phone').keypress(function(event){
 			if(event.which != 8 && isNaN(String.fromCharCode(event.which)) || event.keycode == 32) {
 				event.preventDefault();
 			};
 	});
 
-		$('#continue').submit(function(event){
-			if($('#phone-number').val().length < 10){
+		$('#new_user').onclick(function(event){
+			if($('#phone').val().length < 10){
 				$('.lead').text("Number must be ten digits.");
+				event.preventDefault();
 			} else if ($('#phone-number').val().charAt(0) === "1"){
 				$('.lead').text("Area code and number only, please.");
+				event.preventDefault();
 			} else {
 				$(this).unbind('submit');
 			}
