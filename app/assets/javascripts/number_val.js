43ec6955007b95d1f5023315382462a5e0bf538f
Michael <Michael.ginalick@gmail.com>
2015-07-25 22:38:33 -0500

    added number validation on signup form

--- app/assets/javascripts/number_val.js
+++ app/assets/javascripts/number_val.js
@@ -1,23 +1,30 @@
 $(document).ready(function(){
 
-
 	$('#phone').keypress(function(event){
 			if(event.which != 8 && isNaN(String.fromCharCode(event.which)) || event.keycode == 32) {
 				event.preventDefault();
 			};
 	});
 
-		$('#new_user').onclick(function(event){
-			if($('#phone').val().length < 10){
-				$('.lead').text("Number must be ten digits.");
-				event.preventDefault();
-			} else if ($('#phone-number').val().charAt(0) === "1"){
-				$('.lead').text("Area code and number only, please.");
-				event.preventDefault();
-			} else {
-				$(this).unbind('submit');
+		$('#new_user').click(function(event){
+			if ($('#phone').val().charAt(0) === "1"){			
+				$('#signup').children().first().text("Area code and number only, please.");
+
 			}
 
 		});
 
-})
+		window.onbeforeunload = function() {
+    	window.localStorage.setItem(name, $('#username').val() );
+    	window.localStorage.setItem(email, $('#email').val() ); 
+		}
+
+		window.onload = function() {
+    	var name = window.localStorage.getItem(name);
+    	var email = window.localStorage.getItem(email);
+    	if (name !== null) 
+    		$('#username').val(name);
+    	if (email !== null) 
+    		$('#email').val(email);
+		}
+});
