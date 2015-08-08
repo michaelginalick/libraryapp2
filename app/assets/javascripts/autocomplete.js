942e25b91dbe9c2a79cde9f57a4c27d03c7a2fe1
Michael <Michael.ginalick@gmail.com>
2015-08-07 23:03:42 -0500

    working on autocomplete

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -3,22 +3,19 @@ $(document).ready(function(){
 		$("#bookSearch").autocomplete({
 			source: function( request, response) {
 			var searchContent = $('#bookSearch').val();
-			$.ajax({
-				type: "GET",
-				url: "/books/",
-				data: {
-					search: searchContent
-				},
-					success: function(data){
-						$.each(data, function(key, value) {
-							console.log(key + ":" + value)
-					})
-
-						},
-					'json'
-      }
-				})
-      }
-		})
-	
+			 $.ajax({
+			 	dataType: 'json',
+			 	type: "GET",
+			 	url: "/books/",
+			 	data: {
+			 		search: searchContent
+			 	},
+			 		success: function(data){
+					  var searchResults = $(jqObj).find(".book");
+			 			response($.each(data, function(key, value) {
+			 		}))
+			 	}
+     })
+  	}
+  })
 });
